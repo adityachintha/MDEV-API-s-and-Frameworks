8841209ecfad60c896d7ad828fa92d71b7ba0b86
Aditya Chintha <adityachintha@gmail.com>
2024-09-27 17:57:46 -0400

    set the port and started the server to listen

--- Assignment 1 - Recipes Express API/index.js
+++ Assignment 1 - Recipes Express API/index.js
@@ -5,3 +5,11 @@ const express = require("express");
 // Initialize the express app
 
 const app = express();
+
+// setting a port
+const port = 4000;
+
+// start the server
+app.listen(port, () => {
+  console.log(`server is running on http://locahost:${port}`);
+});
