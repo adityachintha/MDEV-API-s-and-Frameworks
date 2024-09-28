1e394bdb7ddb3861252da03f69e5bd9678cfb160
Aditya Chintha <adityachintha@gmail.com>
2024-09-28 13:37:22 -0400

    Added cookingTime for recipeModel

--- Assignment 1 - Recipes Express API/src/models/recipeModel.js
+++ Assignment 1 - Recipes Express API/src/models/recipeModel.js
@@ -11,4 +11,5 @@ const mongoose = require("mongoose");
 const RecipeSchema = new mongoose.Schema({
   recipeName: { type: String, required: true },
   ingredients: { type: [String], required: true },
+  cookingTime: { type: Number, required: true },
 });
