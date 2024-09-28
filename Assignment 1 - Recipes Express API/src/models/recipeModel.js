336672a25ba2588dafa0d726b3d2fd362f9f452f
Aditya Chintha <adityachintha@gmail.com>
2024-09-28 14:38:48 -0400

    added difficulty and cuisine model

--- Assignment 1 - Recipes Express API/src/models/recipeModel.js
+++ Assignment 1 - Recipes Express API/src/models/recipeModel.js
@@ -12,4 +12,6 @@ const RecipeSchema = new mongoose.Schema({
   recipeName: { type: String, required: true },
   ingredients: { type: [String], required: true },
   cookingTime: { type: Number, required: true },
+  difficulty: { type: String, required: true },
+  cuisine: { type: String, required: true },
 });
