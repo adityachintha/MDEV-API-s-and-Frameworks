e65fcde4c76b100c898a43f4f9c98b6ca3f41b6f
Aditya Chintha <adityachintha@gmail.com>
2024-09-28 15:26:25 -0400

    importing express and router in recipes routes

--- Assignment 1 - Recipes Express API/src/routes/recipeRoutes.js
+++ Assignment 1 - Recipes Express API/src/routes/recipeRoutes.js
@@ -2,3 +2,8 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Data - 09/28/2024
+
+// importing express and router
+
+const express = require("express");
+const router = express.Router();
