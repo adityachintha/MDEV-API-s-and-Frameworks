61ecd8de20dd1399f23bbddc46262272d8d2abd9
Aditya Chintha <adityachintha@gmail.com>
2024-09-28 00:34:20 -0400

    imported body parser

--- Assignment 1 - Recipes Express API/index.js
+++ Assignment 1 - Recipes Express API/index.js
@@ -1,8 +1,14 @@
 // import express
 const express = require("express");
+const bodyparser = require("body-parser");
 
 // Initialize the express app
 const app = express();
+
+//Define a route
+app.get("/", (req, res) => {
+  res.send("Welcome to my first project");
+});
 
 // setting a port
 const port = 4000;
