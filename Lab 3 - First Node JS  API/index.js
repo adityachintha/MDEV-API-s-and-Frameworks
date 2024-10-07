5e1b5c7c42eea79a1eab613683fccecc11a64ed8
Aditya Chintha <adityachintha@gmail.com>
2024-10-07 18:16:24 -0400

    I set the port and added the app to listen from port and start the server

--- Lab 3 - First Node JS  API/index.js
+++ Lab 3 - First Node JS  API/index.js
@@ -1,7 +1,13 @@
 //import expresss
-
 const express = require("express");
 
 //initialize the express app
-
 const app = express();
+
+// set the PORT
+const port = 5000;
+
+//start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
