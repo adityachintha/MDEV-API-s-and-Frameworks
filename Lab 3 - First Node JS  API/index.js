82b1857453fd512dbe757976e7c9a4e5085dbe5a
Aditya Chintha <adityachintha@gmail.com>
2024-10-07 18:25:58 -0400

    I defined a root route to send a response

--- Lab 3 - First Node JS  API/index.js
+++ Lab 3 - First Node JS  API/index.js
@@ -7,6 +7,11 @@ const app = express();
 // set the PORT
 const port = 3000;
 
+//Define a route
+app.get("/", (req, res) => {
+  res.send("Welcome to my first program of nodejs express");
+});
+
 //start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
