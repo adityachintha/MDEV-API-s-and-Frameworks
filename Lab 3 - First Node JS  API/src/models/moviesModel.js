2025357d8d3bfa73fc83324ce9c563ffe302baf8
Aditya Chintha <adityachintha@gmail.com>
2024-10-07 19:07:34 -0400

    imporpting mongoose, adding comment header creating a variable for schema

--- Lab 3 - First Node JS  API/src/models/moviesModel.js
+++ Lab 3 - First Node JS  API/src/models/moviesModel.js
@@ -1,3 +1,9 @@
+// File Name - movieModel.js
+// Student Name - Aditya Chintha
+// Student ID - 200595829
+// Data - 7th October 2024
+
 //import
 
 const mongoose = require("mongoose");
+const schema = mongoose.schema;
