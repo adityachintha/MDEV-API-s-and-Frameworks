3ed7695a72a952630bf0c6ea54062f76b43c991b
Aditya Chintha <adityachintha@gmail.com>
2024-10-08 07:49:54 -0400

    adding the validate movie middlware in the routes for create.

--- Lab 3 - First Node JS  API/src/routes/movieRoutes.js
+++ Lab 3 - First Node JS  API/src/routes/movieRoutes.js
@@ -7,6 +7,7 @@
 const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieController");
+const { validateMovie } = require("../middlewares/movieMiddleware");
 
 //Route to Import Movies
 router.post("/import", movieController.importMovies);
@@ -15,7 +16,7 @@ router.post("/import", movieController.importMovies);
 router.get("/", movieController.getMovies);
 
 //Route to create a new movie
-router.post("/create", movieController.createMovies);
+router.post("/create", validateMovie, movieController.createMovies);
 
 //Route to find a movie by ID
 router.get("/:id", movieController.getMovieById);
@@ -23,7 +24,7 @@ router.get("/:id", movieController.getMovieById);
 //Route to update the movie
 router.put("/update/:id", movieController.updateMovie);
 
-//Route to delte the movie
+//Route to delete the movie
 router.delete("/delete/:id", movieController.deleteMovie);
 
 //export
