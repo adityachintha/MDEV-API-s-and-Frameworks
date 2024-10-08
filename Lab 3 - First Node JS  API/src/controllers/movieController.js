7dbb01b644910a2c87e357c7065fbb4e2098e502
Aditya Chintha <adityachintha@gmail.com>
2024-10-07 21:37:58 -0400

    adding the fs and creating a funtion to import the movies to database

--- Lab 3 - First Node JS  API/src/controllers/movieController.js
+++ Lab 3 - First Node JS  API/src/controllers/movieController.js
@@ -5,3 +5,15 @@
 
 //import the movies
 const Movies = require("../models/moviesModel");
+const fs = require("fs");
+
+//funtion to import movies from json to database
+exports.importMovies = async (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync("./movies.json", "utf-8"));
+    await Movies.insertMany(data); // Import data into Mongo db
+    res.status(200).send("Movies imported Successfully");
+  } catch (e) {
+    console.error(e);
+  }
+};
