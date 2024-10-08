87d997b95d30694bc2ba9aedd831ad3d031bbca2
Aditya Chintha <adityachintha@gmail.com>
2024-10-07 21:43:25 -0400

    importing express, router and moviecontroller to the routes

--- Lab 3 - First Node JS  API/src/routes/movieRoutes.js
+++ Lab 3 - First Node JS  API/src/routes/movieRoutes.js
@@ -2,3 +2,8 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Data - 7th October 2024
+
+//Import
+const express = require("express");
+const router = express.Router();
+const movieController = require("../controllers/movieController");
