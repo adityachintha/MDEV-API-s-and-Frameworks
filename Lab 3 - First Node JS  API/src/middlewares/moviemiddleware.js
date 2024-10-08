4572471ad4eceb4e20a84217fd83bc1ca923b425
Aditya Chintha <adityachintha@gmail.com>
2024-10-08 07:07:13 -0400

    added middleware for logger

--- Lab 3 - First Node JS  API/src/middlewares/moviemiddleware.js
+++ Lab 3 - First Node JS  API/src/middlewares/moviemiddleware.js
@@ -2,3 +2,11 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Data - 7th October 2024
+
+//Middleware to log incoming requests
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method}, ${req.originalURL} - ${new Date().toISOString()}`
+  );
+  next();
+};
