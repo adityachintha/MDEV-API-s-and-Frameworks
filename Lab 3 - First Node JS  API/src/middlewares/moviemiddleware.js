8151dea0b3e4102888ec0e824adab3647841dcca
Aditya Chintha <adityachintha@gmail.com>
2024-10-08 07:46:07 -0400

    updating the typos in middleware

--- Lab 3 - First Node JS  API/src/middlewares/moviemiddleware.js
+++ Lab 3 - First Node JS  API/src/middlewares/moviemiddleware.js
@@ -5,9 +5,7 @@
 
 //Middleware to log incoming requests
 const logger = (req, res, next) => {
-  console.log(
-    `${req.method}, ${req.originalURL} - ${new Date().toISOString()}`
-  );
+  console.log(`${req.method}${req.originalUrl} - ${new Date().toISOString()}`);
   next();
 };
 
