a38164582e9f1d920a440a239995cf8189587d1a
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 02:10:44 -0400

    Updating the database connection code

--- Assignment 2 - Express API Recipes/index.js
+++ Assignment 2 - Express API Recipes/index.js
@@ -7,24 +7,16 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-
 const recipeRoutes = require("./src/routes/recipeRoutes");
-
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 // Initialize the express app
 const app = express();
 
-//Connection to mongodb Atlas
-mongoose
-  .connect(process.env.MONGOURI)
-  .then(() => {
-    console.log("Connected to mongo db");
-  })
-  .catch((error) => {
-    console.error("error connecting to mongodb", error);
-  });
+//Initializing Connection to mongodb Atlas
+const mongoServer = require("./db");
+mongoServer();
 
 //Define a route
 app.use("/", recipeRoutes);
