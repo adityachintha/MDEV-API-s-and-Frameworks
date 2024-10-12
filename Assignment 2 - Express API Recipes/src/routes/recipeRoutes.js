f0d8364e0c87a50bffa3d17b12280df63df6b898
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 02:54:51 -0400

    adding a route to import recipes

--- Assignment 2 - Express API Recipes/src/routes/recipeRoutes.js
+++ Assignment 2 - Express API Recipes/src/routes/recipeRoutes.js
@@ -1,22 +1,16 @@
-// File Name - recipeRoute.js
+// File Name - recipeRoutes.js
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Date - 11 October 2024
 
-// importing express and router
+//Importing express and router
 
 const express = require("express");
 const router = express.Router();
 
-const {
-  addRecipesList,
-  getRecipes,
-} = require("../controllers/recipeController");
+const recipeController = require("../controllers/recipeController");
 
-//route to create recipes from JSON file
-router.post("/import", addRecipesList);
-
-//showing recipes list
-router.get("/", getRecipes);
+//Route to import recipes from JSON file
+router.post("/import", recipeController.importRecipes);
 
 module.exports = router;
