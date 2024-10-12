eb4cdc8857fa4eae101f8f43e44c3664270bb0d4
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 02:28:13 -0400

    added logger for middleware

--- Assignment 2 - Express API Recipes/src/middleware/recipesMiddleware.js
+++ Assignment 2 - Express API Recipes/src/middleware/recipesMiddleware.js
@@ -2,3 +2,9 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Date - 11 October 2024
+
+//Middleware for logger
+const logger = (req, res, next) => {
+  console.log(`${req.method}${req.originalUrl} - ${new Date().toISOString()}`);
+  next();
+};
