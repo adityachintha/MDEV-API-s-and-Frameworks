7c587f0ee3b8707f1a71389139bf30d6270fe828
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 02:05:46 -0400

    initiating the mongo server from the db.js file

--- Assignment 2 - Express API Recipes/db.js
+++ Assignment 2 - Express API Recipes/db.js
@@ -4,3 +4,14 @@ const { default: mongoose } = require("mongoose");
 const databaseUrl = process.env.MONGOURI;
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
+// Initiate the Server
+const mongoServer = async () => {
+  try {
+    await mongoose.connect(databaseUrl);
+    console.log("Connected to Database");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
