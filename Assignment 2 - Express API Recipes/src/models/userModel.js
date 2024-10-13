57236f2148e830d989c8e839313fa440aee2368c
Aditya Chintha <adityachintha@gmail.com>
2024-10-12 23:18:20 -0400

    writing a schema for users

--- Assignment 2 - Express API Recipes/src/models/userModel.js
+++ Assignment 2 - Express API Recipes/src/models/userModel.js
@@ -1,4 +1,10 @@
 //Import modules
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+
+//Schema for Users
+const UserSchema = new mongoose.Schema({
+    username: {type:String, required:true, unique:true},
+    email: {type:String, required:true, unique:true},
+    password: {type:String, required:true}
+});
