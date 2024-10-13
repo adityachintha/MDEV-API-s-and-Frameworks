d6dd0a61a31fdb30c669a8be24b9355b9fc4620c
Aditya Chintha <adityachintha@gmail.com>
2024-10-13 14:46:15 -0400

    importing the necessaries like express, router, user model and user controller

--- Assignment 2 - Express API Recipes/src/routes/userRoutes.js
+++ Assignment 2 - Express API Recipes/src/routes/userRoutes.js
@@ -2,3 +2,11 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Date - 12 October 2024
+
+//Import
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const userController = require("../controllers/userController");
+
+//
