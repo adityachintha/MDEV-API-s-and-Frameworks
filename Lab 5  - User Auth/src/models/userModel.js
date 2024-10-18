a948220ed915c7f5b27901f6926bc4eac036ee6d
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 00:21:37 -0400

    User Schema for users

--- Lab 5  - User Auth/src/models/userModel.js
+++ Lab 5  - User Auth/src/models/userModel.js
@@ -6,3 +6,10 @@
 //Import
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+
+//User Schema for Users
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, //Password doesn't require a unique key
+});
