113b8d56adc8319a3b1c909c1a0861eed0c5803e
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 05:04:21 -0400

    imported the passport, bcrypt , user model and local strtergy

--- Lab 5  - User Auth/src/config/passportConfig.js
+++ Lab 5  - User Auth/src/config/passportConfig.js
@@ -4,3 +4,7 @@
 // Date - 18th October 2024
 
 //Importing
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const LocalStrategy = require("passport-local").Strategy;
