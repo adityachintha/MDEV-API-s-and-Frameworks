39420fe8322917c1acd68766eb85bc4ef626c18b
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 05:15:57 -0400

    added the local statergy configuration for username and password

--- Lab 5  - User Auth/src/config/passportConfig.js
+++ Lab 5  - User Auth/src/config/passportConfig.js
@@ -8,3 +8,27 @@ const passport = require("passport");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const LocalStrategy = require("passport-local").Strategy;
+
+// Local statergy configuration for username/password login
+passport.use(
+  new LocalStrategy(
+    { userField: "username", passwordField: "password" },
+    async (username, password, done) => {
+      try {
+        // Validating if the user exists
+        const user = await User.findOne({ username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect Username" });
+        }
+        //Validating if the password matches
+        const isPassword = await bcrypt.compare(passport, user.passport);
+        if (!isPassword) {
+          return done(null, false, { message: "Incorrect Password" });
+        }
+        return done(null, user);
+      } catch (e) {
+        return done(e);
+      }
+    }
+  )
+);
