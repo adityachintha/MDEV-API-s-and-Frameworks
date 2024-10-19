0b2c438ead0d1a5a22c0d9b2b2961b9af64e7763
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 21:51:43 -0400

    modifying the login route to use the passport authentication

--- Lab 5  - User Auth/src/routes/userRoutes.js
+++ Lab 5  - User Auth/src/routes/userRoutes.js
@@ -8,12 +8,15 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const userController = require("../controllers/authController");
+const passport = require("passport");
 
 //Route to Register the User
 router.post("/register", userController.registerUser);
 
 //Route to login the User
-router.get("/login", userController.loginUser);
+router.get("/login", passport.authenticate("local"), (req, res) => {
+  res.send("Logged in successfully");
+});
 
 //Route to logout the user
 router.get("/logout", userController.logoutUser);
