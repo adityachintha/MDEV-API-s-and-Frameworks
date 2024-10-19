12923c01ca6330b016884f373f16ea59a9abe66a
Aditya Chintha <adityachintha@gmail.com>
2024-10-18 21:20:26 -0400

    added the imports for user Routes

--- Lab 5  - User Auth/src/routes/userRoutes.js
+++ Lab 5  - User Auth/src/routes/userRoutes.js
@@ -2,3 +2,9 @@
 // Student Name - Aditya Chintha
 // Student ID - 200595829
 // Date - 18th October 2024
+
+//Import
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const userController = require("../controllers/authController");
