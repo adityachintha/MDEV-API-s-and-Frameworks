7112bca6f32dc3a7f05a8d56a4b698c0ced908a8
Aditya Chintha <adityachintha@gmail.com>
2024-11-12 20:47:06 -0500

    adding token verification middleware and jsonwebtoken dependency

--- Assignment 3 - Recipes API with JWT Security/src/middleware/auth.js
+++ Assignment 3 - Recipes API with JWT Security/src/middleware/auth.js
@@ -3,7 +3,7 @@
 // Student ID - 200595829
 // Date - 12 November 2024
 
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // Function to verify token.
 // req - The request object param.
@@ -11,19 +11,20 @@ const jwt = require('jsonwebtoken');
 // next - The function called to invoke next object
 // Returns flow to next object on verification success or an error message.
 
-const verifyToken = (req,res,next)=>{
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
 
-    if(!token){
-        return res.status(403).send('Required token for authentication');
-            }
-            try{
-                const decoded = jwt.verify(token,process.env.SECRET_TOKEN);
-                req.user = decoded;
-            } catch(err){
-                return res.status(401).send('Invalid token');
-            }
-            return next();
-}
+  if (!token) {
+    return res.status(403).send("Required token for authentication");
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid token");
+  }
+  return next();
+};
 
 module.exports = verifyToken;
