1644f465788fb665ab087aa1e522786893b54d89
Aditya Chintha <adityachintha@gmail.com>
2024-11-13 18:29:15 -0500

    adding model for recipe

--- Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/Recipe.java
+++ Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/Recipe.java
@@ -1,4 +1,29 @@
 package com.example.assignment3recipesui;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Recipe {
+    @SerializedName("_id")
+    private String id;
+
+    @SerializedName("recipeName")
+    private String recipeName;
+
+    @SerializedName("cookingTime")
+    private String cookingTime;
+
+    @SerializedName("difficulty")
+    private String difficulty;
+
+    @SerializedName("cuisine")
+    private String cuisine;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("photoLink")
+    private String photoLink;
+
+    @SerializedName("averageRating")
+    private double averageRating;
 }
