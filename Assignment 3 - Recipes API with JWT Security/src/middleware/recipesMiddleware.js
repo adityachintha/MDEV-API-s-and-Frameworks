add7ec18c1c20350814bdaf3d19f3a61f1ddb300
Aditya Chintha <adityachintha@gmail.com>
2024-11-12 20:31:42 -0500

    commiting existing recipes middleware

---
+++ Assignment 3 - Recipes API with JWT Security/src/middleware/recipesMiddleware.js
@@ -0,0 +1,17 @@
+// File Name - recipesMiddleware.js
+// Student Name - Aditya Chintha
+// Student ID - 200595829
+// Date - 11 November 2024
+
+//Middleware for logger
+const logger = (req, res, next) => {
+  console.log(`${req.method}${req.originalUrl} - ${new Date().toISOString()}`);
+  next();
+};
+
+//Middleware to handle 404 error
+const handleNotFound = (req, res, next) => {
+  res.status(404).send("Page not found/ something went wrong");
+};
+
+module.exports = { logger, handleNotFound };
