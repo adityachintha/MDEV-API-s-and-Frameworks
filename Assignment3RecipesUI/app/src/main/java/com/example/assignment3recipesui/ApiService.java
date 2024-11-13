371b60841954ff3831f59ed7a5f23bc127245876
Aditya Chintha <adityachintha@gmail.com>
2024-11-13 18:33:15 -0500

    creating api service to call get all recipes api

--- Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/ApiService.java
+++ Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/ApiService.java
@@ -1,4 +1,10 @@
 package com.example.assignment3recipesui;
 
-public class ApiService {
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("recipe/")
+    Call<List<Recipe>> getRecipes();
 }
