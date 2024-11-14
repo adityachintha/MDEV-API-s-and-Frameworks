6b9cd1c6417a85c905f5914bf6264d08680890f4
Aditya Chintha <adityachintha@gmail.com>
2024-11-13 19:39:59 -0500

    adding client to call api service

--- Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/RetrofitClient.java
+++ Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/RetrofitClient.java
@@ -1,4 +1,31 @@
 package com.example.assignment3recipesui;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private static final String BASE_URL = "http://10.0.0.143:3000";
+    private static Retrofit retrofit;
+
+    public static ApiService getApiService() {
+        if (retrofit == null) {
+            // Create an OkHttpClient with timeout settings
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(60, TimeUnit.SECONDS)  // Increase timeout duration
+                    .writeTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient) // Attach the custom client here
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiService.class);
+    }
 }
