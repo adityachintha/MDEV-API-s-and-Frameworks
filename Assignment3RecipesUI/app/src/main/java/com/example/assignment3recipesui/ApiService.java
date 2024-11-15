4be4e3e2035365f9c4317c2aa565fc5ba438a619
Aditya Chintha <adityachintha@gmail.com>
2024-11-16 01:54:07 +0530

    Updated the UI changes to show the recipes List

--- Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/ApiService.java
+++ Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/ApiService.java
@@ -5,6 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    @GET("/recipe/")
+    @GET("/recipes/")
     Call<List<Recipe>> getRecipes();
 }
--- Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/RetrofitClient.java
+++ Assignment3RecipesUI/app/src/main/java/com/example/assignment3recipesui/RetrofitClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://10.0.0.143:3000";
+    private static final String BASE_URL = "https://mdev-api-s-and-frameworks-recipes-ui.onrender.com";
     private static Retrofit retrofit;
 
     public static ApiService getApiService() {
