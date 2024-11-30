685cdd49238998cf40e5ae43ac5bcaed27e32366
Aditya Chintha <adityachintha@gmail.com>
2024-11-30 09:09:21 +0530

    added routes for protected route

--- Lab 6 - FirebaseApp/routes/firebaseRoutes.js
+++ Lab 6 - FirebaseApp/routes/firebaseRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { signUp, signIn, verifyToken } = require("../controllers/firebaseController");
+const {
+  signUp,
+  signIn,
+  verifyToken,
+} = require("../controllers/firebaseController");
 
 const router = express.Router();
 
@@ -9,5 +13,11 @@ router.post("/signup", signUp);
 // Route for sign-in (get Firebase custom token)
 router.post("/signin", signIn);
 
+// Verify Token API (Protected Route Example)
+router.get("/protected", verifyToken, (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Access granted to protected route", user: req.user });
+});
 
 module.exports = router; // Export the router
