0369af3363c98217f6b68ac9eb5d541727178866
Aditya Chintha <adityachintha@gmail.com>
2024-12-06 10:28:50 -0500

    added and updated the login request

--- Assignment 4 - Recipes CRUD UI /app/src/main/java/com/example/assignment3recipesui/LoginRequest.java
+++ Assignment 4 - Recipes CRUD UI /app/src/main/java/com/example/assignment3recipesui/LoginRequest.java
@@ -1,12 +1,45 @@
 package com.example.assignment3recipesui;
 
+import com.google.gson.annotations.SerializedName;
+
 public class LoginRequest {
+    @SerializedName("email")
     private String email;
+
+    @SerializedName("password")
     private String password;
+
+//    @SerializedName("username")
+//    private String username;
 
     public LoginRequest(String email, String password) {
         this.email = email;
         this.password = password;
+//        this.username = username;
     }
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
 }
 
